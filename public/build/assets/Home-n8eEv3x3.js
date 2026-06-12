ca661796f482a6112f1c2143a907c5f3c118ff46
Pandu Satria <138671942+pandustrr@users.noreply.github.com>
2026-06-12 14:44:13 +0700

    testimoni ui auto scroll

---
+++ public/build/assets/Home-n8eEv3x3.js
@@ -0,0 +1,12 @@
+import{j as t}from"./app-C_a_TOz1.js";import{P as x}from"./PublicLayout-BOOE9qXk.js";import u from"./HeroSection-D6hNYdHj.js";import d from"./StatsBar-DSFWrHfW.js";import j from"./RunningTextBanner-WCZG8Vh3.js";import c from"./StickyNewsHighlight-DB53nFSP.js";import y from"./ProfilSection-AZUMjbF7.js";import l from"./TenagaPendidikSection-B0P7Cu2J.js";import S from"./KeunggulanSection-CT5Ia4RX.js";import h from"./BeritaKegiatanSection-DGKZIGug.js";import k from"./FasilitasSection-B3Y3I1TY.js";import P from"./PpdbSection-ND4IzLpG.js";import H from"./CtaSection-BdvjWGUN.js";import T from"./HomeVideo-CpMfSIFd.js";import"./ChevronDownIcon-CH96of35.js";import"./XMarkIcon-Deh7AGo2.js";import"./ArrowLeftIcon-DlS9Vp6J.js";import"./ArrowRightIcon-BCdGGaYI.js";import"./ChevronRightIcon-Dxn6bqI0.js";import"./UserGroupIcon-CnwokU51.js";import"./AcademicCapIcon-WazQuY0b.js";import"./BuildingLibraryIcon-BMUIMRqZ.js";import"./CheckBadgeIcon-BjAeIqtO.js";import"./SparklesIcon-MAK0N1kl.js";import"./CheckCircleIcon-gIsKNiDe.js";import"./ImageGalleryModal-CuyKa2aX.js";import"./PhotoIcon-YDBMg06U.js";import"./PlayIcon-ByyOiVww.js";function b({lembaga:o,prestasi:r=[],kegiatan:w=[],beritas:n=[],stickyBerita:m=null,sidebarSections:e=[],sidebarBeritas:s=[],sidebarCategoryName:p="",pengajars:a=[],fasilitas:f=[],ppdbInfo:i=null}){return t.jsxs(x,{title:o.nama,navTheme:"dark",isLembaga:!0,children:[t.jsx(u,{lembaga:o}),t.jsx(d,{lembaga:o}),t.jsx(j,{text:o.running_text}),t.jsx(c,{stickyBerita:m}),t.jsx(y,{lembaga:o,sidebarSections:e,sidebarBeritas:s,sidebarCategoryName:p}),t.jsx(l,{pengajars:a,lembaga:o}),t.jsx(S,{lembaga:o}),t.jsx(T,{lembaga:o}),t.jsx(h,{beritas:n,prestasi:r,lembaga:o}),t.jsx(P,{lembaga:o,ppdbInfo:i}),t.jsx(k,{lembaga:o,fasilitas:f}),t.jsx(H,{lembaga:o,ppdbInfo:i}),t.jsx("style",{dangerouslySetInnerHTML:{__html:`
+                @keyframes fade-in-up {
+                    from { opacity: 0; transform: translateY(15px); }
+                    to { opacity: 1; transform: translateY(0); }
+                }
+                .animate-fade-in-up {
+                    animation: fade-in-up 0.8s ease-out forwards;
+                }
+                .delay-100 { animation-delay: 0.1s; }
+                .delay-200 { animation-delay: 0.2s; }
+                .delay-300 { animation-delay: 0.3s; }
+            `}})]})}export{b as default};
